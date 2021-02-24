7bcab57aac280f3e6689eb2342c315de0a83ac46
staradamstar <72166226+staradamstar@users.noreply.github.com>
2021-02-24 00:02:34 -0800

    Added Spanish translations.

--- messages/es/alert.php
+++ messages/es/alert.php
@@ -9,13 +9,13 @@
 declare(strict_types=1);
 
 return [
-    'Due to: {reason}' => '',
-    'For SquidTracks or splatnet2statink users:' => '',
-    'Incorrect data will be registered if you do not use the latest version.' => '',
-    'Please update your client software and use always latest version (they will be updated to the latest version when restarted).' => '',
-    'Sorry for inconvenience.' => '',
-    'Term: {startDate} - {endDate}' => '',
-    'We\'ll perform maintenance on the schedule below:' => '',
-    'We won\'t implement automatic importing to {appName} for security reasons.' => '',
-    'You can import automatically from SplatNet 2, use these apps: (USE AT YOUR OWN RISK)' => '',
+    'Due to: {reason}' => 'Debido a: {reason}',
+    'For SquidTracks or splatnet2statink users:' => 'A los usarios de SquidTracks o splatnet2statink:',
+    'Incorrect data will be registered if you do not use the latest version.' => 'Datos incorrectos se registrarán si no usen la versión más reciente',
+    'Please update your client software and use always latest version (they will be updated to the latest version when restarted).' => 'Por favor, actualicen su software de cliente y siempre usen la versión más reciente (se actualizarán al reiniciar).',
+    'Sorry for inconvenience.' => 'Disculpen las molestias.',
+    'Term: {startDate} - {endDate}' => 'Tiempo: {startDate} - {endDate}',
+    'We\'ll perform maintenance on the schedule below:' => 'Realizaremos mantenimiento según el horario por abajo:',
+    'We won\'t implement automatic importing to {appName} for security reasons.' => 'No vamos a implementar la importación automática a {appName} por razones de seguridad.',
+    'You can import automatically from SplatNet 2, use these apps: (USE AT YOUR OWN RISK)' => 'Se puede importar automáticamente de SplatNet 2, usa estas aplicaciones: (ÚSALAS BAJO TU PROPIO RIESGO)',
 ];
